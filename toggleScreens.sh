112b7b680f7b250e4346259b89e91c961c7cf37a
AdamRse <bakaarion3@gmail.com>
2024-12-14 20:33:55 +0100

    ++

--- toggleScreens.sh
+++ toggleScreens.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Définir les noms des sorties vidéo
-MONITOR_LEFT="DP-1"
+MONITOR_LEFT="DP-5"
 MONITOR_MIDDLE="HDMI-0"
-MONITOR_RIGHT="DP-3"
+MONITOR_RIGHT="DP-1"
 
 # Définir la sortie audio, id obtenu via la commande pactl list short sinks
 AUDIO_SINK_TV=$(pactl list short sinks | grep hdmi | awk '{print $1}')
@@ -28,7 +28,6 @@ mode_gaming_canap() {
     AUDIO_SINK_TV=$(pactl list short sinks | grep hdmi | awk '{print $1}')
     echo "audio détecté pour la TV : >$AUDIO_SINK_TV<"
 
-    
     AUDIO_SINK_TV=$(pactl list short sinks | grep hdmi | awk '{print $1}')
 
     # Rediriger l'audio vers la TV
@@ -36,24 +35,20 @@ mode_gaming_canap() {
     pactl set-sink-volume $AUDIO_SINK_TV 100%
     echo "Paramétrage de la sortie audio sur $AUDIO_SINK_TV"
 
-    # Boucle qui retente de récupérer AUDIO_SINK_TV tant qu'il n'est pas trouvé et que le délai n'est pas dépassé. A cause du splitter ou le la TV éteinte, la sortie n'est pas dispo au moment où le programme change d'output vidéo
+    # Boucle qui retente de récupérer AUDIO_SINK_TV
     while [ -z "$AUDIO_SINK_TV" ] && [ $elapsed -lt $timeout ]; do
-        # Récupérer l'ID du sink pour la TV en utilisant le mot-clé "hdmi"
         AUDIO_SINK_TV=$(pactl list short sinks | grep hdmi | awk '{print $1}')
         
-        # Si AUDIO_SINK_TV n'est toujours pas trouvé, attendre une seconde et incrémenter le temps écoulé
         if [ -z "$AUDIO_SINK_TV" ]; then
             echo "Aucune sortie HDMI détectée. Tentative à nouveau dans 1 seconde..."
             sleep 1
             ((elapsed+=1))
         else
-            # Rediriger l'audio vers la TV
             pactl set-default-sink $AUDIO_SINK_TV
             pactl set-sink-volume $AUDIO_SINK_TV 100%
             echo "Paramétrage de la sortie audio sur $AUDIO_SINK_TV"
         fi
     done
-
 
     steam steam://open/bigpicture
 
@@ -62,7 +57,23 @@ mode_gaming_canap() {
     echo "Les écrans $MONITOR_LEFT et $MONITOR_MIDDLE ont été éteints. $MONITOR_RIGHT est maintenant l'écran principal. L'audio est redirigé vers la TV."
 }
 
-# Fonction pour allumer les écrans gauche et milieu en mode "joindre", et remettre l'écran du milieu en principal
+# Fonction pour le mode gaming PC (nouvel écran central uniquement)
+mode_gaming_pc() {
+    # Désactiver les écrans latéraux
+    xrandr --output $MONITOR_LEFT --off
+    xrandr --output $MONITOR_RIGHT --off
+    
+    # Activer l'écran du milieu en tant qu'écran principal
+    xrandr --output $MONITOR_MIDDLE --primary --auto
+
+    # Configurer l'audio sur la sortie bureau
+    AUDIO_SINK_DESK=$(pactl list short sinks | grep analog | awk '{print $1}')
+    pactl set-default-sink $AUDIO_SINK_DESK
+    
+    echo "Mode gaming PC activé : écrans latéraux désactivés, écran central en principal"
+}
+
+# Fonction pour allumer les écrans gauche et milieu en mode "joindre"
 mode_bureau() {
     AUDIO_SINK_DESK=$(pactl list short sinks | grep analog | awk '{print $1}')
 
@@ -75,7 +86,7 @@ mode_bureau() {
     # S'assurer que l'écran de droite est toujours en mode "joindre"
     xrandr --output $MONITOR_RIGHT --auto --right-of $MONITOR_MIDDLE
     
-    # Optionnel : Rediriger l'audio vers la sortie analogique ou autre
+    # Rediriger l'audio vers la sortie analogique
     pactl set-default-sink $AUDIO_SINK_DESK
 
     # Si steam est lancé, quitter le mode big pictures
@@ -87,13 +98,15 @@ mode_bureau() {
     echo "Les écrans $MONITOR_LEFT et $MONITOR_MIDDLE ont été allumés et configurés en mode joindre. $MONITOR_MIDDLE est maintenant l'écran principal. L'audio est redirigé vers la sortie analogique."
 }
 
-#PROGRAMME
+# PROGRAMME
 
 # Vérification de l'argument $1
 if [[ "$1" == "desk" ]]; then
     mode_bureau
 elif [[ "$1" == "tv" ]]; then
     mode_gaming_canap
+elif [[ "$1" == "gaming_desk" ]]; then
+    mode_gaming_pc
 else # Si aucun argument n'est passé, on fait un switch
     if [ -n "$IS_LEFT_ON" ] || [ -n "$IS_MIDDLE_ON" ]; then
         mode_gaming_canap
