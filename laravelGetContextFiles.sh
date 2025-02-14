5df7d932554ed291ac3de0dd690251755a980a87
AdamRse <bakaarion3@gmail.com>
2025-02-14 22:08:00 +0100

    Ajout de laravelGetContextFiles, la copie des controlleurs fonctionne

---
+++ laravelGetContextFiles.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# VARIABLES PARAMETRES
+PREFIXES=("" "$HOME/dev" "$HOME" "$HOME/dev/g404") # Ajouter un préfix dans le tableau pour tester un chemin supplémentaire
+COPY_LOCATION="$HOME/Téléchargements/Contexte_LLM" # Dossier dans lequel sera copié tous les fichiers de contexte
+
+# Vérification du repo
+if [ -z "$1" ]; then
+    echo "paramètre 1 manquant : Il faut le nom ou le répertoire du projet en premier paramètre."
+    exit 1
+fi
+
+# Déclarations et formattage des variables 
+PROJECT_PATH=""
+PATH_SENT="${1%/}"
+PATH_SENT="${PATH_SENT#/}"
+
+# Recherche du chemin absolu du projet
+for PREFIX in "${PREFIXES[@]}"; do
+    TEST_PATH="$PREFIX/$PATH_SENT"
+    if [ -d "$TEST_PATH" ] && [ -f "$TEST_PATH/artisan" ] && [ -f "$TEST_PATH/composer.json" ] && [ -d "$TEST_PATH/app" ]; then
+        PROJECT_PATH="$TEST_PATH"
+        break
+    fi
+done
+
+if [ -z "$PROJECT_PATH" ]; then
+    echo "Impossible de trouver le projet $1"
+    exit 1
+fi
+echo "Projet Laravel trouvé : dans $PROJECT_PATH"
+
+if [ -d "$COPY_LOCATION" ]; then
+    echo "Nettoyage du répertoire de contexte"
+    rm -rf "$COPY_LOCATION"
+else
+    echo "Création du répertoire de contexte"
+    mkdir -p "$COPY_LOCATION"
+fi
+
+# Copie et modification des contrôleurs
+echo "Copie des contrôleurs"
+find "$PROJECT_PATH/app/Http/Controllers" -name "*.php" -type f | while read -r file; do
+    # Calcul du chemin relatif
+    rel_path=${file#$PROJECT_PATH/}
+    
+    # Récupération du nom du fichier sans le chemin
+    filename=$(basename "$file")
+    
+    # Lecture du fichier et ajout du chemin relatif après <?php
+    awk '/<\?/{print;print "// Chemin relatif: '"$rel_path"'";next}1' "$file" > "$COPY_LOCATION/$filename"
+done
