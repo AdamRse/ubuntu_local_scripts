8fd42f28f09cf105e1b0a48a5487cfa89f152341
AdamRse <bakaarion3@gmail.com>
2025-02-17 03:08:10 +0100

    laravelGetContextFiles V1.0

--- laravelGetContextFiles.sh
+++ laravelGetContextFiles.sh
@@ -3,12 +3,20 @@
 # VARIABLES PARAMETRES
 PREFIXES=("" "$HOME/dev" "$HOME" "$HOME/dev/g404") # Ajouter un préfix dans le tableau pour tester un chemin supplémentaire
 COPY_LOCATION="$HOME/Téléchargements/Contexte_LLM" # Dossier dans lequel sera copié tous les fichiers de contexte
+FILES_TO_COLLECT=( # Tableau de string qui contient les extensions et répertoires à collecter. ex : "php resources/views" va collecter tous le fichiers .php dans le répertoire resources/views du projet (récursivement)
+    "php app"
+    "php resources/views"
+    "css public"
+    "web.php routes"
+    "php database/migrations"
+    "php database/seeders"
+)
 
 # Fonctions
 copy_files_with_path() { # Utilisation : copy_files_with_path "<extension>,<extension>" "<chemin_relatif>"; exemple : copy_files_with_path "js,css" "public"; ou : copy_files_with_path "php" "app/Http/Controllers"
     local EXTENSIONS="$1"
     local RELATIVE_PATH="$2"
-    echo "Copie des fichiers depuis $RELATIVE_PATH pour extensions: $EXTENSIONS"
+    echo -e "-----------\nRecherche et copie des fichiers depuis $RELATIVE_PATH pour extensions: $EXTENSIONS"
     
     # Création du tableau d'extensions
     IFS=',' read -ra EXT_ARRAY <<< "$EXTENSIONS"
@@ -19,24 +27,39 @@ copy_files_with_path() { # Utilisation : copy_files_with_path "<extension>,<exte
     
     for ext in "${EXT_ARRAY[@]}"; do
         if $first; then
-            find_params+=(-name "*.$ext")
+            find_params+=(-name "*$ext")
             first=false
         else
-            find_params+=(-o -name "*.$ext")
+            find_params+=(-o -name "*$ext")
         fi
     done
     
     find "$PROJECT_PATH/$RELATIVE_PATH" \( "${find_params[@]}" \) -type f | while read -r file; do
-        echo "Fichier trouvé : $file"
+        echo "$file"
         # Calcul du chemin relatif
         rel_path=${file#$PROJECT_PATH/}
         # Récupération du nom du fichier sans le chemin
         filename=$(basename "$file")
-        # Lecture du fichier et ajout du chemin relatif après <?php
-        if [[ "$file" == *.php ]]; then
-            awk '/<\?/{print;print "// File location in project : '"$rel_path"'";next}1' "$file" > "$COPY_LOCATION/$filename"
+
+        if [[ "$filename" == *.blade.php ]]; then
+            # Pour les fichiers Blade
+            echo -e "{{-- File location in project : $rel_path --}}\n\n" > "$COPY_LOCATION/$filename"
+            cat "$file" >> "$COPY_LOCATION/$filename"
+        elif [[ "$filename" == *.php ]]; then
+            # Pour les fichiers PHP standards
+            awk '/<\?/{print;print "// File location in project : '"$rel_path"'\n\n";next}1' "$file" > "$COPY_LOCATION/$filename"
+        elif [[ "$filename" == *.js ]]; then
+            # Pour les fichiers JavaScript
+            echo -e "// File location in project : $rel_path\n\n" > "$COPY_LOCATION/$filename"
+            cat "$file" >> "$COPY_LOCATION/$filename"
+        elif [[ "$filename" == *.css ]]; then
+            # Pour les fichiers CSS
+            echo -e "/*\n* File location in project : $rel_path\n*/\n\n" > "$COPY_LOCATION/$filename"
+            cat "$file" >> "$COPY_LOCATION/$filename"
         else
-            cp "$file" "$COPY_LOCATION/$filename"
+            # Pour tous les autres types de fichiers
+            echo -e "# File location in project : $rel_path\n\n" > "$COPY_LOCATION/$filename"
+            cat "$file" >> "$COPY_LOCATION/$filename"
         fi
     done
 }
@@ -77,19 +100,10 @@ else
     mkdir -p "$COPY_LOCATION"
 fi
 
-# Copie et modification des contrôleurs
-# echo "Copie des contrôleurs"
-# copy_files_with_path "php" "app/Http/Controllers"
-
-declare -A FILES_TO_COPY=(
-    ["controllers"]="php app/Http/Controllers"
-    ["public_assets"]="css,js public"
-    # ["public_assets2"]="js public"
-)
-
-for key in "${!FILES_TO_COPY[@]}"; do
-    # Séparation des valeurs
-    read -r extension path message <<< "${FILES_TO_COPY[$key]}"
-    # Appel de la fonction avec les deux paramètres
+# On parcourt le tableau FILES_TO_COLLECT en extrayant l'extension à rechercher, et le chemin où chercher
+for entry in "${FILES_TO_COLLECT[@]}"; do
+    read -r extension path <<< "$entry"
     copy_files_with_path "$extension" "$path"
 done
+
+echo -e "++++++++\nTerminé !"
