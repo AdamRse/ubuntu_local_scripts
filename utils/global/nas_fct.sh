6a48096a15359a83c152e7d0d2da8290ede8aa13
AdamRse <adamrse@gmail.com>
2025-06-18 18:23:12 +0200

    mount_nas fonctionne

--- utils/global/nas_fct.sh
+++ utils/global/nas_fct.sh
@@ -17,27 +17,17 @@ mount_nas() {
 
     echo "Création du point de montage dans $NAS_MOUNT_POINT"
     # Créer le point de montage local
-    if ! mkdir -p "$NAS_MOUNT_POINT"; thencapt
+    if ! mkdir -p "$NAS_MOUNT_POINT"; then
         echo "Impossible de créer le point de montage."
         return 1
     fi
     echo "Point de montage créé"
-
-    # Monter via SSHFS en utilisant SSH_ASKPASS
-    export SSH_ASKPASS=/tmp/sshfs_askpass_$$
-    echo "#!/bin/bash" > "$SSH_ASKPASS"
-    echo "echo \"$NAS_PASSWORD\"" >> "$SSH_ASKPASS"
-    chmod +x "$SSH_ASKPASS"
     
     echo "Tentative de montage du serveur NAS..."
-    if setsid sshfs -o port="$NAS_PORT" -o reconnect -o ServerAliveInterval=15 -o ServerAliveCountMax=3 "$NAS_USER@$NAS_ADDR:/" "$NAS_MOUNT_POINT"; then
+    if sshfs -o default_permissions,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,compression=yes,cache_timeout=3600 -p "$NAS_PORT" "$NAS_USER@$NAS_ADDR:/" "$NAS_MOUNT_POINT"; then
         echo "Montage réussi!"
-        rm -f "$SSH_ASKPASS"
-        echo "NAS monté sur $NAS_MOUNT_POINT. Contenu :"
-        ls "$NAS_MOUNT_POINT"
     else
         echo "Impossible de monter le serveur NAS $NAS_NAME"
-        rm -f "$SSH_ASKPASS"
         return 1
     fi
 }
